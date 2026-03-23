50567bd5c295aa238b700deaeb237c1a25ce8308
cakeonfire <frank.pos.ziel@gmail.com>
2026-03-23 16:33:39 +0100

    add tree object for class heierarchy representation

---
+++ cmdkit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+
+
+
+
+
+void cmd_loop(void) {
+
+    while (true) {
+
+    }
+
+}
+
+/*
+int main(void) {
+
+
+    return 0;
+}
+*/
--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <fstream>
 
+#include "systree.h"
 
-
+using namespace std;
 
 
 int main(void) {
+    //cout << "test" << "\n";
 
+    TreeNode* root = tree_new_node(nullptr, "root");
+    tree_print(root, 0);
+
+    TreeNode* child1 = tree_new_node(root, "1");
+    TreeNode* child2 = tree_new_node(root, "2");
+    tree_print(root, 0);
+
+    tree_free(&root);
 
     return 0;
 }
