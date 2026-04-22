fb91d0fe9a03fbfbf94da840ee9e4e6fa6578cce
Indexception <indexception@gmail.com>
2026-04-22 13:48:07 +0200

    simplify attributes

--- entity.h
+++ entity.h
@@ -8,25 +8,29 @@ class Entity {
     private:
         static int _id_inc;
         const int _entity_id;
+        string name;  // treat like ID?
 
     protected:
-        Entity(double max_h, double health, double damage, int speed);
-        Entity(double health, double damage, int speed);
+        //Entity(double max_h, double health, double damage, int speed);
+        //Entity(double health, double damage, int speed);
+        Entity(string& name, double max_h, double health, double damage);
+        Entity(string& name, double health, double damage);
         ~Entity();
 
-        int speed;
+        //int speed;
         double damage;
         double max_health;
         double health;
 
     public:
-        int get_speed(void) const;
+        //int get_speed(void) const;
+        const string& get_name(void) const;  // if treated like ID, there should be no way of changing the name
         int get_damage(void) const;
         int get_max_health(void) const;
         int get_health(void) const;
         virtual string get_species(void) const;
 
-        void set_speed(int new_val);
+        //void set_speed(int new_val);
         void set_damage(double new_val);
         void set_max_health(double new_val);
         void set_health(double new_val);
--- player_entity.h
+++ player_entity.h
@@ -6,33 +6,33 @@
 class PlayerEntity : public Entity {
     // NOTE redistribute stats (defense, agility, accuracy) for specific subclasses, also allows to get rid of weapon levels
     // additional stats: agility, accuracy, defense
-    // subclasses should implement special stat variations
 
     protected:
-        PlayerEntity(string name, int level, double max_h, double health, double damage, int speed, int defense, int agility, int accuracy);
+        //PlayerEntity(string name, int level, double max_h, double health, double damage, int speed, int defense, int agility, int accuracy);
+        PlayerEntity(string name, int level, double max_h, double health, double damage);
         ~PlayerEntity();
 
-        string name;
         int level;
-        int defense;
-        int agility;
-        int accuracy;
+        //int defense;
+        //int agility;
+        //int accuracy;
 
     public:
-        const string& get_name(void) const;
+        // const string& get_name(void) const;
         // REVIEW it might make no sense to override get_label for each player subclass as it uses common vars
-        virtual string get_label(void) const;  // string label of name + race etc.
+        // also since we have print_info, maybe just remove labels entirely
+        virtual string get_label(void) const;  // string label of name + species etc.
         int get_level(void) const;
-        int get_defense(void) const;
-        int get_agility(void) const;
-        int get_accuracy(void) const;
+        //int get_defense(void) const;
+        //int get_agility(void) const;
+        //int get_accuracy(void) const;
         string get_species(void) const override;
 
-        void set_name(string new_name);
+        //void set_name(string new_name);
         void set_level(int new_level);
-        void set_defense(int new_val);
-        void set_agility(int new_val);
-        void set_accuracy(int new_val);
+        //void set_defense(int new_val);
+        //void set_agility(int new_val);
+        //void set_accuracy(int new_val);
 
         void print_info(void) const override;
 };
@@ -40,61 +40,69 @@ class PlayerEntity : public Entity {
 // Player entities
 class Human : public PlayerEntity {
     private:
-        int sword_level;
+        //int sword_level;
+        int strength;
 
     protected:
 
     public:
-        Human(string name, int level, double max_h, double health, double damage, int speed, int defense, int agility, int accuracy, int sword_level);
+        Human(string name, int level, double max_h, double health, double damage, int strength);
         ~Human();
 
-        int get_sword_level(void) const;
+        //int get_sword_level(void) const;
+        int get_strength(void) const;
         string get_label(void) const override;
         string get_species(void) const override;
 
-        void set_sword_level(int new_sword_level);
+        //void set_sword_level(int new_sword_level);
+        void set_strength(int new_str);
 
         void print_info(void) const override;
 };
 
 class Dwarf : public PlayerEntity {
     private:
-        int axe_level;
+        //int axe_level;
+        int toughness;
 
     protected:
-    // all specialized levels can be in private as there's no more inheriting classes
     //int axe_level;
 
     public:
-        Dwarf(string name, int level, double max_h, double health, double damage, int speed, int defense, int agility, int accuracy, int axe_level);
+        Dwarf(string name, int level, double max_h, double health, double damage, int toughness);
         ~Dwarf();
 
-        int get_axe_level(void) const;
+        //int get_axe_level(void) const;
+        int get_toughness(void) const;
         string get_label(void) const override;
         string get_species(void) const override;
 
-        void set_axe_level(int new_axe_level);
+        //void set_axe_level(int new_axe_level);
+        void set_toughness(int new_toughness);
 
         void print_info(void) const override;
 };
 
 class Elf : public PlayerEntity {
     private:
-        int bow_level;
-        int range;
+        //int bow_level;
+        //int range;
+        int agility;
 
     protected:
 
     public:
-        Elf(string name, int level, double max_h, double health, double damage, int speed, int defense, int agility, int accuracy, int bow_level, int range);
+        Elf(string name, int level, double max_h, double health, double damage, int agility);
         ~Elf();
 
-        int get_bow_level(void) const;
-        int get_range(void) const;
+        //int get_bow_level(void) const;
+        //int get_range(void) const;
+        int get_agility(void) const;
         string get_label(void) const override;
         string get_species(void) const override;
 
-        void set_bow_level(int new_bow_level);
+        //void set_bow_level(int new_bow_level);
+        void set_agility(int new_agility);
 
         void print_info(void) const override;
 };
